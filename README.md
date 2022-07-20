7481beea2037a990844213e2732d1ed3c806f04b
Claudia Donahue <claudialdonahue@gmail.com>
2022-07-20 16:41:27 -0400

    cleaned/set up Stroke data for analysis

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ This project uses data on aviation accidents within an R Shiny app, allowing a u
 App users need the following packages installed before running the app:  
 - tidyverse  
 - shiny  
+- caret  
 
 The following code will install the required packages:  
 ```
-install.packages(c("tidyverse", "shiny"))
+install.packages(c("tidyverse", "shiny", "caret"))
 ```
 
 The code below will allow the user to run the app from RStudio using `shiny::runGitHub()`:  
--- Strokes/server.R
+++ Strokes/server.R
@@ -8,9 +8,25 @@ library(caret)
 library(tidyverse)
 library(DT)
 
-# Read in data
+################# Read in and clean the data #################
 data <- readr::read_csv(file = "../strokeData.csv",
                         show_col_types = FALSE)
+# Make some married column into factors
+data$ever_married <- factor(data$ever_married)
+# drop id column
+data <- dplyr::select(data, -id)
+# change bmi to a number, and "N/A" to NA
+data$bmi <- as.numeric(data$bmi)
+
+# Make some dummy variables
+# gender, work_type, Residence_type, smoking_status
+dummies <- dummyVars(age ~ ., data = data)
+addDummy <- data.frame(predict(dummies, newdata = data))
+# keep the columns I want
+data <- data.frame(cbind(data[,c(2:5, 8:9,11)], addDummy[,c(1,3,8:14, 17:20)]))
+# Reorder columns so stroke is last 
+columnOrder <- c(1:6, 8:20, 7)
+data <- data[, columnOrder]
 
 
 # Define server logic required to draw a histogram
